b213b92c5566d5fe9e783f5e4ba2e70a45b32147
resent <contact@resent.dev>
2024-02-14 14:09:35 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Image, { StaticImageData } from "next/image";
 import styles from "./page.module.css";
-import Profile from '../../public/profile.jpg'
-import RaidHub from '../../public/RaidHub.png'
-import Felony from '../../public/Felony.jpg'
-import Loti from '../../public/Loti.gif'
+import Profile from '../../public/profile.png'
+import Grief from '../../public/grief.png'
 import { FaGithub } from "react-icons/fa";
 import { FaDiscord } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
@@ -19,15 +17,15 @@ export default function Home() {
           <span className={styles['description']}>I&apos;m a Software Engineer & Web Developer.</span>
         </div>
         <div className={styles['socials']}>
-          <a href='https://github.com/NotBruce' className={styles['social']}>
+          <a href='https://github.com/evincement' className={styles['social']}>
             <FaGithub className={styles['social-icon']} />
             <span className={'social-name'}>GitHub</span>
           </a>
-          <a href='https://discord.com/users/771891131499675668' className={styles['social']}>
+          <a href='https://discord.com/users/334593049319571456' className={styles['social']}>
             <FaDiscord className={styles['social-icon']} />
             <span className={'social-name'}>Discord</span>
           </a>
-          <a href='mailto:me@notbruce.dev' className={styles['social']}>
+          <a href='mailto:contact@slit.sh' className={styles['social']}>
             <MdEmail className={styles['social-icon']} />
             <span className={'social-name'}>Email</span>
           </a>
@@ -36,22 +34,16 @@ export default function Home() {
       <span className={styles['projects-title']}>Projects</span>
       <div className={styles['projects']}>
         <Project
-          title="RaidHub"
-          description="A fast website that provides raid statistics like completions, lowmans and much more to come"
-          image={RaidHub}
-          link='https://raidhub.io/'
+          title="dev"
+          description=" "
+          image={Profile}
+          link='https://resent.dev'
         />
         <Project
-          title="Loti"
+          title="grief"
           description="Versatile Discord Bot For Moderation, Social Commands, and More!"
-          image={Loti}
-          link='https://www.loti.dev/'
-        />
-        <Project
-          title="Felony"
-          description="Versatile Discord Bot For Moderation, Social Commands, and More!"
-          image={Felony}
-          link='https://felony.me/'
+          image={Grief}
+          link='https://grief.cloud'
         />
       </div>
     </div>
