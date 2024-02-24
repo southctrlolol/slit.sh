bd5cac58a34374d464fafa63442176b50de2b27d
resent <contact@resent.dev>
2024-02-24 09:41:44 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Yor from '../../public/yor.png'
 import { FaGithub } from "react-icons/fa";
 import { FaDiscord } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
+import Resent from '../../public/resent.png'
 
 
 export default function Home() {
@@ -22,7 +23,36 @@ export default function Home() {
             <FaGithub className={styles['social-icon']} />
             <span className={'social-name'}>GitHub</span>
           </a>
+          <a href='https://discord.com/users/334593049319571456' className={styles['social']}>
+            <FaDiscord className={styles['social-icon']} />
+            <span className={'social-name'}>Discord</span>
+          </a>
+          <a href='mailto:contact@slit.sh' className={styles['social']}>
+            <MdEmail className={styles['social-icon']} />
+            <span className={'social-name'}>Email</span>
+          </a>
+      <span className={styles['projects-title']}>Projects</span>
+      <div className={styles['projects']}>
+        <Project
+          title="dev"
+          description="Versatile Discord Bot For Moderation, Social Commands, and More."
+          image={Resent}
+          link='https://resent.dev'
+        />
+        <Project
+          title="grief"
+          description="Versatile Discord Bot For Moderation, Social Commands, and More."
+          image={Grief}
+          link='https://grief.cloud'
+        />
+        <Project
+          title="yor"
+          description="a fun active community for meeting new friends"
+          image={Yor}
+          link='https://discord.gg/yor'
+        />
       </div>
+    </div>
   );
 }
 
@@ -30,4 +60,10 @@ const Project = ({ title, description, image, link }: { title: string, descripti
   return (
     <a className={styles['project']} href={link} >
       <Image src={image} alt={title}/>
+      <div className={styles['project-info']}>
+        <span className={styles['project-title']}>{title}</span>
+        <span className={styles['project-description']}>{description}</span>
+      </div>
+    </a>
+  );
 }
