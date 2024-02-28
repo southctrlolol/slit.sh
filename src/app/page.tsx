d8ff6c891dcb74061086a92fb8248d18b1b6fc3a
resent <contact@resent.dev>
2024-02-27 19:00:06 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,5 @@ export default function Home() {
           <a href='mailto:contact@slit.sh' className={styles['social']}>
             <MdEmail className={styles['social-icon']} />
             <span className={'social-name'}>Email</span>
-            </a>
-        </div>
- );
-}
+            );
+            }
