fb3c5f7d4ef9c08712f63ce223bfd1cc6fe4a0fb
southctrlolol <ctrlisinfloridalol@gmail.com>
2024-08-04 20:15:37 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,41 @@
 import Image, { StaticImageData } from "next/image";
 import styles from "./page.module.css";
-import Chippy(2) from '../../public/Chippy(2).png'
-import Terrify from '../../public/terrify.png'
-import Cemetery from '../../public/cemetery.gif'
+import Chippy from '../../public/Chippy.png';
 import { FaGithub } from "react-icons/fa";
 import { FaDiscord } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-import Chippy from '../../public/Chippy.png'
-
 
 export default function Home() {
   return (
-    <div className={styles['main']}>
-      <div className={styles['me']}>
-        <Image src={Profile} alt="Avatar" />
+    <div className={styles.main}>
+      <div className={styles.me}>
+        <Image src={Chippy} alt="Avatar" />
         <div className={styles['me-right']}>
-            <span className={styles['name']}>Hello, I&apos;m sin</span>
-          <span className={styles['description']}>I&apos;m a bot developer.</span>
+          <span className={styles.name}>Hello, I'm sin</span>
+          <span className={styles.description}>I'm a bot developer.</span>
         </div>
-        <div className={styles['socials']}>
-          <a href='https://github.com/evincement' className={styles['social']}>
+        <div className={styles.socials}>
+          <a href='https://github.com/evincement' className={styles.social}>
             <FaGithub className={styles['social-icon']} />
-            <span className={'social-name'}>GitHub</span>
+            <span className={styles['social-name']}>GitHub</span>
           </a>
-          <a href='https://discord.com/users/1252001166703853588' className={styles['social']}>
+          <a href='https://discord.com/users/1252001166703853588' className={styles.social}>
             <FaDiscord className={styles['social-icon']} />
-            <span className={'social-name'}>Discord</span>
+            <span className={styles['social-name']}>Discord</span>
           </a>
-          <a href='mailto:contact@slit.sh' className={styles['social']}>
+          <a href='mailto:contact@slit.sh' className={styles.social}>
             <MdEmail className={styles['social-icon']} />
-            <span className={'social-name'}>Email</span>
+            <span className={styles['social-name']}>Email</span>
           </a>
         </div>
       </div>
       <span className={styles['projects-title']}>Projects</span>
-      <div className={styles['projects']}>
+      <div className={styles.projects}>
         <Project
           title="Chippy"
           description="versatile Discord Bot For moderation, social commands, and more"
-          image={Grief}
+          image={Chippy}
           link='https://discord.gg/CNSYxykkVg'
-        />
-        <Project
-          title="Chippy(2)"
-          description="versatile Discord Bot For moderation, social commands, and more"
-          image={Resent}
-          link='https://discord.gg/CNSYxykkVg'
-        />
-        <Project
-          title="terrify"
-          description="an active friendly community server"
-          image={Cemetery}
-          link='https://discord.gg/terrify'
         />
       </div>
     </div>
@@ -60,8 +44,8 @@ export default function Home() {
 
 const Project = ({ title, description, image, link }: { title: string, description: string, image: StaticImageData, link: string }) => {
   return (
-    <a className={styles['project']} href={link} >
-      <Image src={image} alt={title}/>
+    <a className={styles.project} href={link} >
+      <Image src={image} alt={title} />
       <div className={styles['project-info']}>
         <span className={styles['project-title']}>{title}</span>
         <span className={styles['project-description']}>{description}</span>
